92b5a6c1b328350bc17d4e4cea5c0bb377263633
Mojmira <mielke.magdalena99@gmail.com>
2021-04-18 23:30:19 +0200

    CharacterAdapter

--- app/src/main/java/com/example/guildwars2app/CharacterDetails.kt
+++ app/src/main/java/com/example/guildwars2app/CharacterDetails.kt
@@ -1,4 +1,4 @@
 package com.example.guildwars2app
 
-class CharacterDetails(var race:String, var gender: String, var profesion:String, var level:Int, var age:Int, var created: String) {
+class CharacterDetails(var name:String, var race:String, var gender: String, var profesion:String, var level:Int, var age:Int, var created: String) {
 }
